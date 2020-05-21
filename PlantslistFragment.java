7de44720d3a648761c50ed03b51c42076c37e838
KIM-JOOHYUN <64013256+KIM-JOOHYUN@users.noreply.github.com>
2020-05-21 16:49:49 +0900

    200519_Update
    My Plants_Test1
    0519.ver

---
+++ PlantslistFragment.java
@@ -0,0 +1,24 @@
+package com.example.test1;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link PlantslistFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class PlantslistFragment extends Fragment {
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_plantslist, container, false);
+    }
+}
